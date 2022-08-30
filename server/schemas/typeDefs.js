160ec1bae77ba8e8038f8d45890ccee4eb764416
bortkyle18 <bortkyle18@gmail.com>
2022-08-30 13:22:51 -0400

    typeDef starter

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -17,16 +17,48 @@ const typeDefs = gql`
     friends: [User]
   }
 
+  type Actor {
+    _id: ID
+    name: String
+    image: String
+  }
+
+  type Movie {
+    _id: ID
+    title: String
+    year: Int
+    image: String
+    genres: String
+    tagline: String
+    imdbRating: Int
+    metaCriticRating: Int
+    plot: String
+    actors: [Actor]
+    username: String
+  }
+
+  type Category {
+    _id: ID
+    title: String
+    description: String
+    username: String
+    movieCount: Int
+    movies: [Movie]
+  }
+
   type Query {
     me: User
     users: [User]
     user(username: String!): User
+    categories (username: String): [Category]
+    category(_id: ID!): Category
   }
 
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
     addFriend(friendId: ID!): User
+    addCategory(title: String!, description: String!, movies: [Movie]!): Category
   }
 `;
 
